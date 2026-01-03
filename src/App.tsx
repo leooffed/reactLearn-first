aa17b5b6b6a6c1b1e45b3b1516413bf6b1267d14
leo libres <developpeurclear@gmail.com>
2026-01-03 15:13:48 +0100

    Mettre a jour l'affichage

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
+import { useState } from "react"
 
 
 const App = () => {
-  function handleClick() {
-    return alert('Button clicked!');
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
   }
+
   return (
     <div>
        <button onClick={handleClick}>
-        Clique ici
+        Clique {count} fois
        </button>
     </div>
   )
